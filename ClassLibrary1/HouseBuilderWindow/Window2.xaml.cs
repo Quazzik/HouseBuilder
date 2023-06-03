237b10dc1b9e7995c7bc3585d0daccd797bb4901
Quazzik <Yuakorotkov@gmail.com>
2023-06-03 18:07:23 +0500

    patch 1.00.04
    added reports

--- ClassLibrary1/HouseBuilderWindow/Window2.xaml.cs
+++ ClassLibrary1/HouseBuilderWindow/Window2.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FastReport;
+using FastReport.Export.Image;
+using FastReport.Export.PdfSimple;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -47,6 +50,37 @@ namespace HouseBuilderWindow
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void Report_Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<Data> reportData = new();
+            reportData.Add(new Data { Name = "Общая площадь дома", Value = Convert.ToInt32(Label_CalculateTotalArea.Content) });
+            reportData.Add(new Data { Name = "Общая стоимость строительных материалов", Value = Convert.ToInt32(Label_CalculateMaterialCost.Content) });
+            reportData.Add(new Data { Name = "Стоимость работы", Value = Convert.ToInt32(Label_CalculateLaborCost.Content) });
+            reportData.Add(new Data { Name = "Стоимость всех разрешений", Value = Convert.ToInt32(Label_CalculatePermitCost.Content) });
+            reportData.Add(new Data { Name = "Общая стоимость отделочных материалов", Value = Convert.ToInt32(Label_CalculateFinishingCost.Content) });
+            reportData.Add(new Data { Name = "Общая стоимость доп. опций", Value = Convert.ToInt32(Label_CalculateOptionCost.Content) });
+            reportData.Add(new Data { Name = "общая стоимость строительстваы", Value = Convert.ToInt32(Label_CalculateTotalCost.Content) });
+            reportData.Add(new Data { Name = "ежемесячный платеж по кредиту", Value = Convert.ToInt32(Label_CalculateMonthlyPayment.Content) });
+            reportData.Add(new Data { Name = "расчёт необходимых накоплений для оплаты строительства + резервный фонд", Value = Convert.ToInt32(Label_CalculateTotalSavingsNeeded.Content) });
+            reportData.Add(new Data { Name = "сколько нужно откладывать каждый месяц чтобы скопить сумму к началу строительства", Value = Convert.ToInt32(Label_CalculateMonthlySavings.Content) });
+            Report report = new();
+            report.Load("Table.frx");
+            report.RegisterData(reportData, "Data");
+            report.Prepare();
+
+            report.SavePrepared("Prepared_Table.fpx");
+
+            ImageExport image = new();
+            image.ImageFormat = ImageExportFormat.Jpeg;
+            report.Export(image, "report.jpg");
+
+            PDFSimpleExport pdfExport = new();
+
+            pdfExport.Export(report, "report.pdf");
+
+            report.Dispose();
         }
     }
 }
